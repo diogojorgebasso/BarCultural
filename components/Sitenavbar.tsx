0758d424ae9c6902d0de562801c0889e170e6ea7
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-02-16 12:25:10 -0300

    add UI package, decided for Next

---
+++ components/Sitenavbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
+import { LogoBarCultural } from "./LogoBarCultural";
+import { useTheme as useNextTheme } from 'next-themes'
+import { Switch, useTheme } from '@nextui-org/react'
+
+
+export default function Sitenavbar(){
+
+  const { setTheme } = useNextTheme();
+  const { isDark } = useTheme();
+
+
+    return(
+        <Navbar isBordered variant="floating">
+        <Navbar.Brand>
+          <LogoBarCultural />
+          <Text b color="inherit" hideIn="xs">
+            ACME
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs" variant="highlight-rounded">
+          <Navbar.Link href="#">Festas</Navbar.Link>
+          <Navbar.Link isActive href="#">Filiação</Navbar.Link>
+          <Navbar.Link href="#">Parceiros</Navbar.Link>
+          <Navbar.Link href="#">Sobre nós</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content>
+          <Navbar.Item>
+          <Switch
+        checked={isDark}
+        onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+      />
+          </Navbar.Item>
+          <Navbar.Item>
+            <Button auto flat as={Link} href="#">
+              Sign Up
+            </Button>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+    )
+}
