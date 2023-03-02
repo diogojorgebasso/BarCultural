4c1199774bae52727f4d35721b633f92f2bb4de2
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-03-02 15:21:22 -0300

    test: functions
    Signed-off-by: Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,18 @@
 const functions = require('firebase-functions');
-import { getStorage, ref } from "firebase/storage";
-const admin = require('firebase-admin');
-admin.initializeApp();
+//import { getStorage, ref } from "firebase/storage";
+//const admin = require('firebase-admin');
+//admin.initializeApp();
 
 
-export const gerarIngresso = functions.firestore
-  .document('ingresso/{docId}')
-  .onWrite((snap, context) => { 
-    const value = snap.data();
-    const email = value.email;
-    const hash = value.hash;
-   });
+//export const gerarIngresso = functions.firestore
+//  .document('ingresso/{docId}')
+//  .onWrite((snap, context) => { 
+//    const value = snap.data();
+//    const email = value.email;
+//   const hash = value.hash;
+//   });
 
+export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+  });
