9d49cdd2fabf5e43a51fd063baee525a100d9d43
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-03-06 01:08:55 -0300

    trying to fix functions
    Signed-off-by: Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>

---
+++ components/AppCheckProviderFirebase.tsx
@@ -0,0 +1,20 @@
+import React, {FC} from "react"
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+import {    useFirebaseApp, 
+    AppCheckProvider} from "reactfire"
+
+const APP_CHECK_TOKEN = "6LdqCZIjAAAAAEs75exDvAH-Q4Q4XNTNyh-aamn2";
+
+export default function AppCheckProviderFirebase({ children }){
+    const appFirebase = useFirebaseApp(); // a parent component contains a `FirebaseAppProvider`
+    const appCheck = initializeAppCheck(appFirebase, {
+      provider: new ReCaptchaV3Provider(APP_CHECK_TOKEN),
+      isTokenAutoRefreshEnabled: true,
+    });
+  return (
+    <AppCheckProvider sdk={appCheck}>
+        {children}
+    </AppCheckProvider>
+  )
+};
+
